1dd1c3efcd583becc8400fb0bf51047c6d9815c1
downy-dev <129946619+downy-dev@users.noreply.github.com>
2023-08-01 21:08:53 +0900

    Add files via upload

---
+++ KYC-Verification-Consulting-System/kyc/KYC-Verification-Consulting-System/product1/product1Metamask.js
@@ -0,0 +1,29 @@
+let connectedAccount = null;
+
+function connectToMetaMask() {
+    // MetaMask가 설치되어 있는지 확인
+    if (typeof window.ethereum !== 'undefined') {
+      // MetaMask와 연결
+      ethereum.request({ method: 'eth_requestAccounts' })
+        .then((accounts) => {
+          // 연결된 계정 정보를 가져옴
+          connectedAccount = accounts[0];
+          alert('MetaMask와 연결되었습니다. 연결된 계정: ' + connectedAccount);
+          // 이후에는 connectedAccount 변수를 사용하여 메타마스크와 상호작용 가능
+          displayAccountAddress(connectedAccount);
+        })
+        .catch((error) => {
+          alert('MetaMask 연결에 실패했습니다. 오류: ' + error.message);
+        });
+    } else {
+      alert('MetaMask를 설치하고 브라우저에 활성화해주세요.');
+    }
+}
+
+function displayAccountAddress(connectedAccount) {
+    const accountAddressElement = document.getElementById('metamaskAddress');
+    accountAddressElement.textContent = "⭐ "+ connectedAccount +" ⭐";
+}
+
+const connectButton = document.getElementById('metamaskAddress');
+connectButton.addEventListener('click', connectToMetaMask);
