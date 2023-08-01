1c7f223839991daa8544705974216bf6e43b0572
downy-dev <129946619+downy-dev@users.noreply.github.com>
2023-08-01 20:06:05 +0900

    Add files via upload

---
+++ kyc/KYC-Verification-Consulting-System/facialKYC/Venous.js
@@ -0,0 +1,28 @@
+const video = document.getElementById('video');
+
+function startWebcam() {
+    navigator.mediaDevices.getUserMedia({ 
+        video: true,
+        audio : false,
+    })
+    .then(stream => {
+        video.srcObject = stream;
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+
+startWebcam();
+
+function showButton() {
+    const conditionalButtonContainer = document.getElementById("conditionalButtonContainer");
+    const button = document.createElement("button");
+    button.id = "conditionalButton";
+    button.textContent = "Verification Success!!";
+    conditionalButtonContainer.appendChild(button);
+}
+
+
+const interval = 10000; // 1초마다 조건을 평가합니다.
+setTimeout(showButton, interval);
